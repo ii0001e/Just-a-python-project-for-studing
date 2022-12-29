c3a607e8f410c7aa4a0d8655e6dbf04c3eccb9bf
Ivan Ivanenko <ivan.ivanenko1996@gmail.com>
2022-12-29 19:57:51 +0200

    listld

--- Just a python project for studing/kodutöö/matteadmistekontroll.py
+++ Just a python project for studing/kodutöö/matteadmistekontroll.py
@@ -40,6 +40,48 @@ for i in range(attempt):
         c = choice(level_3)
         question = "{}{}{}".format(a,c,b)
         answer = input(f"Sisesta vastus: {a}{c}{b}  -->  ")
+from random import *
+level_1 = ("+","-")
+level_2 = ["+","-","/","*"]
+level_3 = ["+","-","/","*","**"]
+
+try:
+    level_choise = int(input("Sisesta, millise raskusega ülesanned tahaksite tesitda? Sisata 1, 2 voi 3 --->   "))
+    attempt = int(input("Mitu ülesannet sa tahad korrata: "))
+except ValueError:
+    print("Siseta ainult arv")
+
+for i in range(attempt):
+    if level_choise == 1:
+        a = randint(1,100)
+        b = randint(-100,100)
+        c = choice(level_1)
+        question = "{}{}{}".format(a,c,b)
+        print(question)
+        answer = input(f"Sisesta vastus: {a}{c}{b}  -->  ")
+        print (answer)
+        if int(answer) == eval(question):
+            i += 1
+            print("Oige!")
+        else: 
+            print("Vale!")
+    elif level_choise == 2:
+        a = randint(1,100)
+        b = randint(1,100)
+        c = choice(level_2)
+        question = "{}{}{}".format(a,c,b)
+        answer = input(f"Sisesta vastus: {a}{c}{b}  -->  ")
+        if int(answer) == eval(question):
+            i += 1
+            print("Oige!")
+        else: 
+            print("Vale!")
+    else:
+        a = randint(1,100)
+        b = randint(1,100)
+        c = choice(level_3)
+        question = "{}{}{}".format(a,c,b)
+        answer = input(f"Sisesta vastus: {a}{c}{b}  -->  ")
         if int(answer) == eval(question):
             i += 1
             print("Oige!")
