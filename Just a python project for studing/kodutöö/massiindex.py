e0ef990da05e6b2a43339b48c2bc66acf9b82014
Ivan Ivanenko <ivan.ivanenko1996@gmail.com>
2023-01-10 17:05:25 +0200

    massiindex parandatud

--- Just a python project for studing/kodutöö/massiindex.py
+++ Just a python project for studing/kodutöö/massiindex.py
@@ -4,14 +4,17 @@ def massiind():
     print(f"{nimi}, oi kui ilus nimi!")
     choose = int(input(f"{nimi}! Kas leian Sinu keha indeksi? 0-ei, 1-jah => "))
     if choose == 1:
-        try:
-            pikkus = int(input("Sisesta oma pikkus CM-s. => "))
-            while pikkus < 0 or pikkus > 273:
+        pikkus = input("Sisesta oma pikkus CM-s. => ")
+        while pikkus.isalpha():
+            print("Pikkus peab olema rohkem kui null ja vähem kui 273. Ja kirjutatud numbritega!")
+            pikkus = input("Sesesta oma pikkus CM-s. => ")
+        while float(pikkus) < 0 or float(pikkus) > 273:
+            try:
                 print("Pikkus peab olema rohkem kui null ja vähem kui 273. Ja kirjutatud numbritega!")
                 pikkus = input("Sesesta oma pikkus CM-s. => ")
-        except:
-            pikkus = 170
-            print("Siis pikkus on 170 cm.")
+            except:
+                pikkus = 170
+                print("Siis pikkus on 170 cm.")
         try:
             mass = float(input("Siseta oma keha kaal KG-s. => "))
             while mass < 0:
@@ -22,8 +25,8 @@ def massiind():
         except:
             mass = 69
             print("Siis teie mass on 69 kg.")
-        index = round((mass / (0.01 * pikkus)**2), 1)
-        print(f"{nimi}! Sinu keha indeks on: {index}")
+        index = round((float(mass) / (0.01 * float(pikkus))**2), 1)
+        print(f"\n{nimi}! Sinu keha indeks on: {index}")
         if index <= 16:
             answer = "Tervisele ohtlik alakaal"
         elif 16 < index <= 19:
@@ -40,6 +43,7 @@ def massiind():
             answer = "Tervisele ohtlik rasvumine"     
     else:
         answer = "Kahju! See on väga kasulik info!" + "\n"
-    print (answer)
-    print(f"Kohtumiseni, {nimi}! Igavesti Sinu, Python!")
-#massiind()
+    print (f"\n{answer}")
+    print()
+    print(f"Kohtumiseni, {nimi}! \nSinu, Python!")
+massiind()
